6b974f1d2319b425b0d9dd144c20a9822081c34b
islam0122 <duishobaevislam01@gmail.com>
2025-09-15 19:21:50 +0600

    add routers

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import asyncio
 from aiogram import Bot,Dispatcher
+from .islam_handlers import islam_router
 from .nazima03_handlers import nazima03_router
-
+from .aijarkyn_handlers import aijarkyn_router
+from .aisara_handlers import aisara_router
+from .Aibiike_handlers import Aibiike_router
+from .yrys_handlers import yrys_router
 TOKEN = "7427361367:AAGKMJ1hU9YnqY_hLtIPFNU4HclqcnReBl0gi"
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 dp.include_router(nazima03_router)
+dp.include_router(islam_router)
+dp.include_router(aijarkyn_router)
+dp.include_router(aisara_router)
+dp.include_router(Aibiike_router)
+dp.include_router(yrys_router)
 
 
 async def startup():
