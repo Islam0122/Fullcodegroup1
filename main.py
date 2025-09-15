8f7141a3a127564fbd1afa70a63d9632d34c1291
islam0122 <duishobaevislam01@gmail.com>
2025-09-15 15:38:39 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import asyncio
+from aiogram import Bot,Dispatcher
+
+TOKEN = "8227707236:AAFlQlr8OvklYp9v8hbOvOkQ3FaTimbpVhQ"
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+async def startup():
+    print("----------------------- i woke up BOSS ---------------------------")
+
+async def off():
+    print(" ----------------------i asleep BOSS -----------------------------")
+
+async def main():
+    dp.startup.register(startup)
+    dp.shutdown.register(off)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
